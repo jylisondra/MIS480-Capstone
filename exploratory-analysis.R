735fe1d3d6972ed3741c73214c2d3a4361edbd15
jylisondra <82999511+jylisondra@users.noreply.github.com>
2021-05-09 11:14:15 -0700

    Initial exploratory analysis and graphical output
    Explores relationship between ridership and various other variables

--- exploratory-analysis.R
+++ exploratory-analysis.R
@@ -3,4 +3,4 @@ plot(Year,Ridership_Total)
 plot(Year,Ridership_Total,type="o")
 plot(Registered_Vehicles_LA,Ridership_Total,main="Registered Vehicles vs Ridershp Total",pch=19)
 plot(Gas_Year_Avg,Ridership_Total,main="Avg Yearly Gas Price vs Ridership Total",pch=19)
-plot(Customer_Satisfaction_Overall,Ridership_Total,main="Overall Customer Satisfaction vs Ridershp Total",pch=19)pr
+plot(Customer_Satisfaction_Overall,Ridership_Total,main="Overall Customer Satisfaction vs Ridershp Total",pch=19)
