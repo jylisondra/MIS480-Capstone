3a61826b3dc1d2c808d42ceb185e4e9d953ef099
jylisondra <82999511+jylisondra@users.noreply.github.com>
2021-05-09 11:11:11 -0700

    exploratory-analysis.R
    R code for initial exploratory analysis

---
+++ exploratory-analysis.R
@@ -0,0 +1,6 @@
+attach(my_data)
+plot(Year,Ridership_Total)
+plot(Year,Ridership_Total,type="o")
+plot(Registered_Vehicles_LA,Ridership_Total,main="Registered Vehicles vs Ridershp Total",pch=19)
+plot(Gas_Year_Avg,Ridership_Total,main="Avg Yearly Gas Price vs Ridership Total",pch=19)
+plot(Customer_Satisfaction_Overall,Ridership_Total,main="Overall Customer Satisfaction vs Ridershp Total",pch=19)pr
